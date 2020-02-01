ea0ca275fc0a9a9257e8b6eceb77db90da1b4a09
wlrcyr1 <59192746+wlrcyr1@users.noreply.github.com>
2020-02-01 16:05:15 +0800

    settings

--- blogsys/settings.py
+++ blogsys/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'blogsys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'abc123',
+        'HOST': '',
+        'port': '',
     }
 }
 
