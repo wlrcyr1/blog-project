05d0530346851ffdc003e74aee89501d4c558f21
wlrcyr1 <59192746+wlrcyr1@users.noreply.github.com>
2020-02-01 20:55:10 +0800

    settings

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import *
 # Register your models here.
+admin.site.register(UserInfo)
+admin.site.register(Blog)
+admin.site.register(UserFans)
+admin.site.register(Category)
+admin.site.register(ArticleDetail)
+admin.site.register(UpDown)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Article)
+admin.site.register(Article2Tag)
